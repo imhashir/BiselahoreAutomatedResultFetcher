644184f2fff63a257d2da6aa07df38275c6164e8
Hashir Baig <hashirbaig@gmail.com>
2017-08-22 08:52:47 +0500

    Added slow page loading retry loop

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from splinter import Browser
+from splinter import exceptions
+
+import xlsxwriter
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-import xlsxwriter
-
 
 inputFilename = input("Enter Input Filename: ")
 outFilename = input("Enter Output Filename: ")
@@ -32,20 +33,37 @@ browser.visit('http://www.biselahore.com/')
 for line in file:
 	browser.fill('student_rno', line)
 	browser.find_by_name('submit').click()
-	#TODO: Wait here
 	browser.windows.current = browser.windows[1]
-	while(browser.status_code != 200):
-		x = x
 	print("Rollno" + line)
-	worksheet.write(row, col, line)
+	worksheet.write(row, col, int(line))
 	col = col + 1
-	worksheet.write(row, col, browser.find_by_tag('td')[8].text)
+	
+	retry = True
+	while retry:
+		try:
+			worksheet.write(row, col, browser.find_by_tag('td')[8].text)
+			retry = False
+		except IndexError:
+			print("Retrying Name...")
+			retry = True
+		except exceptions.ElementDoesNotExist:
+			print("Retrying Name...")
+			retry = True
+	
 	col = col + 1
 	for index in range(8):
-		worksheet.write(row, col, browser.find_by_tag('td')[24 + (index*3)].text)
+		retry = True
+		while retry:
+			try:
+				worksheet.write(row, col, int(browser.find_by_tag('td')[24 + (index*3)].text))
+			except IndexError:
+				retry = True
+			except exceptions.ElementDoesNotExist:
+				retry = True
+			else:
+				retry = False
+				
 		col = col + 1
-		#print(browser.find_by_tag('td')[23 + (index*3)].text + "\n")
-		#print(browser.find_by_tag('td')[24 + (index*3)].text + "\n")
 	col = 0
 	row = row + 1
 	browser.windows.current = browser.windows[0]
