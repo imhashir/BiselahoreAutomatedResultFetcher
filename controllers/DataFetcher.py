6e89f64a7988535e9bef8eaed3f7aa173a66bb67
Hashir Baig <hashirbaig@gmail.com>
2018-07-21 12:07:07 +0500

    Updated Bot to work with site design of July 21, 2018

--- controllers/DataFetcher.py
+++ controllers/DataFetcher.py
@@ -3,77 +3,86 @@ from splinter import exceptions
 import xlsxwriter
 from xlsxwriter.utility import xl_rowcol_to_cell
 from xlsxwriter.utility import xl_col_to_name
+import sys
+
 
 class DataFetcher:
-		
-	def fetchData(self, inputFilename, outputFilename):
-		print("Preparing Spreadsheet...")
-		
-		try:
-			file = open(inputFilename, "r")
-		except FileNotFoundError:
-			print("No file named '" + inputFilename + "' exists in folder 'input'")
-			sys.exit()
-		
-		workbook = xlsxwriter.Workbook("output/" + outputFilename + '.xlsx')
-		worksheet = workbook.add_worksheet()
 
-		x = 0
-		row = 1
-		col = 0
+    def fetchData(self, inputFilename, outputFilename):
+        print("Preparing Spreadsheet...")
 
-		print("Initiating Automation...\n")
-		browser = Browser()
-		browser.visit('http://result.biselahore.com/')
-		browser.fill('rollNum', file.readline())
-		browser.find_by_value("View Result").click()
-		file.seek(0)
+        try:
+            file = open(inputFilename, "r")
+        except FileNotFoundError:
+            print("No file named '" + inputFilename + "' exists in folder 'input'")
+            sys.exit()
 
-		worksheet.write(0, 0, "Roll No")
-		worksheet.write(0, 1, "Name")
-		worksheet.set_column(xl_col_to_name(1) + ":" + xl_col_to_name(1), 24)
+        workbook = xlsxwriter.Workbook("output/" + outputFilename + '.xlsx')
+        worksheet = workbook.add_worksheet()
 
-		for i in range(8):
-			worksheet.write(0, i + 2, browser.find_by_tag('td')[24 + i*4].text)
-		worksheet.write(0, 10, "Total")
-		browser.back()
+        x = 0
+        row = 1
+        col = 0
 
-		print("Fetching Result Data...\n")
+        print("Initiating Automation...\n")
+        browser = Browser()
+        browser.visit('http://biselahore.com/')
+        browser.fill('student_rno', file.readline())
+        browser.find_by_value("Get Result").click()
+        file.seek(0)
 
-		for line in file:
-			browser.fill('rollNum', line)
-			browser.find_by_value("View Result").click()
-			print("Rollno" + line)
-			worksheet.write(row, col, str(line))
-			col = col + 1
-			
-			retry = True
-			while retry:
-				try:
-					worksheet.write(row, col, browser.find_by_tag('td')[11].text)
-					retry = False
-				except IndexError:
-					retry = True
-				except exceptions.ElementDoesNotExist:
-					retry = True
-			
-			col = col + 1
-			for index in range(8):
-				retry = True
-				while retry:
-					try:
-						worksheet.write(row, col, int(browser.find_by_tag('td')[26 + (index*4)].text))
-					except IndexError:
-						retry = True
-					except exceptions.ElementDoesNotExist:
-						retry = True
-					else:
-						retry = False
-				col = col + 1
-			worksheet.write(row, col, "=SUM(" + xl_rowcol_to_cell(row, 2) + ":" + xl_rowcol_to_cell(row, col - 1) + ")")
-			col = 0
-			row = row + 1
-			browser.back()
+        worksheet.write(0, 0, "Roll No")
+        worksheet.write(0, 1, "Name")
+        worksheet.set_column(xl_col_to_name(1) + ":" + xl_col_to_name(1), 24)
 
-		workbook.close()
-		browser.quit()
+        for i in range(8):
+            word = browser.find_by_tag('td')[26 + i * 6].text
+            worksheet.write(0, i + 2, word)
+        worksheet.write(0, 10, "Total")
+        browser.back()
+
+        print("Fetching Result Data...\n")
+
+        for line in file:
+            browser.fill('student_rno', line)
+            browser.find_by_value("Get Result").click()
+            print("Rollno: " + line)
+            worksheet.write(row, col, str(line))
+            col = col + 1
+
+            retry = True
+            while retry:
+                try:
+                    name = browser.find_by_tag('td')[8].text
+                    print(name)
+                    worksheet.write(row, col, name)
+                    retry = False
+                except IndexError:
+                    retry = True
+                except exceptions.ElementDoesNotExist:
+                    retry = True
+
+            col = col + 1
+            for index in range(8):
+                retry = True
+                while retry:
+                    try:
+                        marks = browser.find_by_tag('td')[30 + (index * 6)].text
+                        try:
+                            worksheet.write(row, col, int(marks))
+                        except ValueError:
+                            worksheet.write(row, col, 0)
+                    except IndexError:
+                        retry = True
+                    except exceptions.ElementDoesNotExist:
+                        retry = True
+                    else:
+                        retry = False
+                col = col + 1
+            worksheet.write(row, col, "=SUM(" + xl_rowcol_to_cell(row, 2) + ":" + xl_rowcol_to_cell(row, col - 1) + ")")
+            col = 0
+            row = row + 1
+            browser.back()
+
+        workbook.close()
+        browser.quit()
--- main.py
+++ main.py
@@ -4,25 +4,27 @@ from layouts import layout_main
 from controllers import DataFetcher
 import _thread
 
-if os.path.isdir("output") == False: 
-	os.mkdir("output")
+if not os.path.isdir("output"):
+    os.mkdir("output")
+
 
 class AppFrame(layout_main.MainFrame):
-	def __init__(self, parent):
-		layout_main.MainFrame.__init__(self, parent)
-	
-	def onClickStart(self, event):
-		print("Preparing Spreadsheet...")
-		inputFilename = self.inputFilePicker.GetPath()
-		outputFilename = self.outputFilename.GetValue()
-		print(inputFilename)
-		print(outputFilename)
-		fetcher = DataFetcher.DataFetcher()
-		try:
-			_thread.start_new_thread(fetcher.fetchData, (inputFilename, outputFilename))
-		except:
-			print("Unable to start a thread")
-			
+    def __init__(self, parent):
+        layout_main.MainFrame.__init__(self, parent)
+
+    def onClickStart(self, event):
+        print("Preparing Spreadsheet...")
+        inputFilename = self.inputFilePicker.GetPath()
+        outputFilename = self.outputFilename.GetValue()
+        print(inputFilename)
+        print(outputFilename)
+        fetcher = DataFetcher.DataFetcher()
+        try:
+            _thread.start_new_thread(fetcher.fetchData, (inputFilename, outputFilename))
+        except:
+            print("Unable to start a thread")
+
+
 app = wx.App(False)
 frame = AppFrame(None)
 frame.Show(True)
